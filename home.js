43e2b8c57fbe8ee3b07e2431d72d5fb37377f042
kurt <kurtdl133@gmail.com>
2024-03-04 18:03:20 +0800

    login

--- home.js
+++ home.js
@@ -1,7 +1,24 @@
 
-let myname;
-document.getElementById("mysubmit").onclick = function(){
-    myname = document.getElementById("text").value;
-    document.getElementById("result").textContent = myname;
-}
+let email = "kurtdl133@gmail.com";
+let password = "12345";
 
+let subEmail, subPassword;
+
+function verify(){
+    subEmail = document.getElementById("email").value;
+    subPassword = document.getElementById("password").value;
+
+
+    if(subEmail == email){
+        if(subPassword == password){
+            document.getElementById("status").textContent = "Login Succesfully";
+        }
+        else{
+            document.getElementById("status").textContent = "Wrong Password";
+
+        }
+    } else{
+        document.getElementById("status").textContent = "Wrong Email";
+
+    }
+}
