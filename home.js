e702460f8ea4610ad51d8c104d4047979df19544
kurt <kurtdl133@gmail.com>
2024-03-04 16:39:52 +0800

    Trial1

--- home.js
+++ home.js
@@ -1,5 +1,7 @@
 
-
-function pushme(){
-    
+let myname;
+document.getElementById("mysubmit").onclick = function(){
+    myname = document.getElementById("text").value;
+    document.getElementById("result").textContent = myname;
 }
+
